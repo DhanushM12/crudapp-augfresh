4c84c3d877a6d39d0811e5e9c9105cb78b5f4fca
DhanushM12 <dhanushmukhi@gmail.com>
2023-04-03 22:01:16 +0530

    Added create and get all

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const port = 8080;
 
 const db = require('./config/mongoose')
 
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>REST APIs for CRUD App</h1>')
+})
+
+const subscriberRouter = require('./routes/index');
+
+app.use('/subscriber', subscriberRouter);
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running server ${err}`)
